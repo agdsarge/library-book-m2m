28d334418e5959b004113962064105e5ed42a9b3
agdsarge <agdsargeant@gmail.com>
2020-04-27 12:25:58 -0400

    fixed library errors and created exception

--- Library.rb
+++ Library.rb
@@ -1,17 +1,20 @@
 class Library
     attr_reader :name, :street_address, :city, :state, :has_ILL
     @@all = []
-    def initialize(name, street_address, city, has_ILL, state=VA)
-        raise "" unless
+    def initialize(name, street_address, city, has_ILL, state="VA")
+        raise "ERROR" unless [name, street_address, city, state].all? {|arg| arg.class == String}
         @name = name
         @street_address = street_address
         @city = city
         @state = state
         @has_ILL = has_ILL
+        @@all << self
     end
-
 
     def self.all
         @@all
     end
 end
+
+# wrl = Library.new("WRL", "Baker St", "Williamsburg", true, "VA")
+# puts wrl.name
