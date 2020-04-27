b03041cb49ad0ff8f7960044d361d6188cc88634
epittab <ep7m@me.com>
2020-04-27 12:24:48 -0400

    fixing parameter name; excluding ? from canSShare

--- Book.rb
+++ Book.rb
@@ -2,9 +2,9 @@ class Book
 
     @@all = []
 
-    attr_reader :title, :author, :genre, :pub_date, :page_count, :canShare?
+    attr_reader :title, :author, :genre, :pub_date, :page_count, :canShare
 
-    def initialize(title, author, genre, pub_date, page_count, canShare?)
+    def initialize(title, author, genre, pub_date, page_count, canShare)
         # puts method(:initialize).parameters
         # raise "Danger, Will Robinson!" unless method(:initialize).parameters.all? { |elem| elem[1].class == Symbol } # +> array of parameters
         @title = title
@@ -12,7 +12,7 @@ class Book
         @genre = genre
         @pub_date = pub_date
         @page_count = page_count
-        @canShare? = canShare?
+        @canShare = canShare
         @@all << self
     end
 
@@ -27,6 +27,6 @@ class Book
 end
 
 
-hp = Book.new("title_diddledee", "author_diddledee", "genre_diddledee", "pub_date_diddledee", "page_count_diddledee")
+hp = Book.new("title_diddledee", "author_diddledee", "genre_diddledee", "pub_date_diddledee", "page_count_diddledee", true)
 
 puts hp.title
